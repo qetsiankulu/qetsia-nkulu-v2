ed0ed1ccf1694aa7e9d9e2ee00294ef26e2696d1
Qetsia Nkulu <qetsiankulu@gmail.com>
2025-03-11 20:57:08 -0700

    Changes to config file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // This enables static export
-    distDir: 'out',   // Output folder
-    images: {
-      unoptimized: true // GitHub Pages doesn't support Next.js image optimization
-    },
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
